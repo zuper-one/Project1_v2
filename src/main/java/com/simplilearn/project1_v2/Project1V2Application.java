9555d489cfe243543bbae62e440c25fe41b35dc4
Ubuntu <ubuntu@ip-172-31-20-237.us-east-2.compute.internal>
2021-11-28 03:31:28 +0000

    Add logic and test

---
+++ src/main/java/com/simplilearn/project1_v2/Project1V2Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.project1_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1V2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/project1_v2/Project1V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.project1_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
